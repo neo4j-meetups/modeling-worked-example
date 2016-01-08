fdc312f4b3205b45f3bb071c649be8067f66c42c
Mark Needham <mark.needham@neotechnology.com>
2016-01-08 21:59:37 +0000

    more queries

--- lib/download_groups.py
+++ lib/download_groups.py
@@ -2,11 +2,14 @@ import requests
 import os
 from collections import Counter
 import json
+from pprint import pprint
 
 key =  os.environ['MEETUP_API_KEY']
+lat = "51.5072"
+lon = "0.1275"
 
 seed_topic = "nosql"
-uri = "https://api.meetup.com/2/groups?&topic={0}&lat=51.5072&lon=0.1275&key={1}".format(seed_topic, key)
+uri = "https://api.meetup.com/2/groups?&topic={0}&lat={1}&lon={2}&key={3}".format(seed_topic, lat, lon, key)
 
 r = requests.get(uri)
 all_topics = [topic["urlkey"]  for result in r.json()["results"] for topic in result["topics"]]
@@ -16,10 +19,10 @@ topics = [entry[0] for entry in c.most_common(10)]
 
 groups = {}
 for topic in topics:
-    uri = "https://api.meetup.com/2/groups?&topic={0}&lat=51.5072&lon=0.1275&key={1}".format(topic, key)
+    uri = "https://api.meetup.com/2/groups?&topic={0}&lat={1}&lon={2}&key={3}".format(topic, lat, lon, key)
     r = requests.get(uri)
     for group in r.json()["results"]:
         groups[group["id"]] = group
 
 with open('data/groups.json', 'w') as groups_file:
-    json.dump(groups.values(), groups_file)
+    json.dump(groups.values(), groups_file, indent=4, sort_keys=True)
--- lib/download_members.py
+++ lib/download_members.py
@@ -4,6 +4,8 @@ from collections import Counter
 import json
 
 key =  os.environ['MEETUP_API_KEY']
+lat = "51.5072"
+lon = "0.1275"
 
 with open('data/groups.json') as group_file:
     groups_json = json.load(group_file)
@@ -11,9 +13,16 @@ with open('data/groups.json') as group_file:
 groups = [str(group['id']) for group in groups_json]
 
 for group in groups:
+    members_file = "data/members/{0}.json".format(group)
+    if os.path.isfile(members_file):
+        print "Members {0}: Already processed [{1}]".format(group, os.stat(members_file).st_size)
+        continue
+    else:
+        print "Members {0}: Processing".format(group)
+
     print "processing group {0}".format(group)
     results = []
-    uri = "https://api.meetup.com/2/members?&group_id={0}&lat=51.5072&lon=0.1275&key={1}".format(str(group), key)
+    uri = "https://api.meetup.com/2/members?&group_id={0}&lat={1}&lon={2}&key={3}".format(str(group),lat,lon, key)
     while True:
         if uri is None:
             break
@@ -21,5 +30,5 @@ for group in groups:
         for result in response["results"]:
             results.append(result)
         uri = response["meta"]["next"] if response["meta"]["next"] else None
-    with(open("data/members/{0}.json".format(group), 'w')) as members_file:
+    with(open(members_file, 'w')) as members_file:
         json.dump(results, members_file)
