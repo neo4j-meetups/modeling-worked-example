9389b68f7c868403ad7a00e607505989d5185d5f
Michael Hunger <github@jexp.de>
2016-04-02 21:38:44 +0200

    updated render script and guide
    * csv-url points to github
    * pass in base-url to guide generator
    * fixed several cypher statements

--- docs/render.sh
+++ docs/render.sh
@@ -1,6 +1,6 @@
 GUIDES=../../neo4j-guides
 # git clone http://github.com/jexp/neo4j-guides $GUIDES
-
-$GUIDES/run.sh 01_groups_by_topics.adoc 01_groups_by_topics.html +1 http://localhost:8001
+BASE_URL=${1-http://localhost:8001}
+$GUIDES/run.sh 01_groups_by_topics.adoc 01_groups_by_topics.html +1 $BASE_URL
 
 python $GUIDES/http.py
