af155032dd09834faab11b2c311e24547dea5fa5
Mark Needham <mark.needham@neotechnology.com>
2016-04-02 23:15:15 +0100

    moved membership out into its own guide

--- docs/render.sh
+++ docs/render.sh
@@ -1,6 +1,7 @@
 GUIDES=../../neo4j-guides
 # git clone http://github.com/jexp/neo4j-guides $GUIDES
 BASE_URL=${1-http://localhost:8001}
-$GUIDES/run.sh 01_groups_by_topics.adoc 01_groups_by_topics.html +1 $BASE_URL
+$GUIDES/run.sh 01_similar_groups_by_topic.adoc 01_similar_groups_by_topic.html +1 $BASE_URL
+$GUIDES/run.sh 02_recommend_me_similar_groups.adoc 02_recommend_me_similar_groups.html +1 $BASE_URL
 
 python $GUIDES/http.py
