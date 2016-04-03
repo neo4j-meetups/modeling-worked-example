38b337cfa72de9612ba3144d7503b8d3dc7b0dee
Mark Needham <mark.needham@neotechnology.com>
2016-04-03 17:00:37 +0100

    render them as well

--- docs/render.sh
+++ docs/render.sh
@@ -7,5 +7,7 @@ $GUIDES/run.sh 03_my_interests.adoc 03_my_interests.html +1 $BASE_URL
 $GUIDES/run.sh 04_events.adoc 04_events.html +1 $BASE_URL
 $GUIDES/run.sh 05_layered_events.adoc 05_layered_events.html +1 $BASE_URL
 $GUIDES/run.sh 06_procedures.adoc 06_procedures.html +1 $BASE_URL
+$GUIDES/run.sh 07_real_time_recommendations.adoc 07_real_time_recommendations.html +1 $BASE_URL
+$GUIDES/run.sh 08_free_for_all.adoc 08_free_for_all.html +1 $BASE_URL
 
 python $GUIDES/http.py
