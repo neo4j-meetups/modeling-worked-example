60f72a8bfb4e4e3f3fb921a0ff0d16b40d422483
Mark Needham <mark.needham@neotechnology.com>
2016-04-03 16:23:24 +0100

    more docs

--- docs/render.sh
+++ docs/render.sh
@@ -3,5 +3,9 @@ GUIDES=../../neo4j-guides
 BASE_URL=${1-http://localhost:8001}
 $GUIDES/run.sh 01_similar_groups_by_topic.adoc 01_similar_groups_by_topic.html +1 $BASE_URL
 $GUIDES/run.sh 02_my_similar_groups.adoc 02_my_similar_groups.html +1 $BASE_URL
+$GUIDES/run.sh 03_my_interests.adoc 03_my_interests.html +1 $BASE_URL
+$GUIDES/run.sh 04_events.adoc 04_events.html +1 $BASE_URL
+$GUIDES/run.sh 05_layered_events.adoc 05_layered_events.html +1 $BASE_URL
+$GUIDES/run.sh 06_procedures.adoc 06_procedures.html +1 $BASE_URL
 
 python $GUIDES/http.py
--- import.sh
+++ import.sh
@@ -1,24 +1,26 @@
 #!/bin/sh
 
-NEO="neo4j-community-2.3.2"
-# NEO="neo4j-community-3.0.0-M03"
+# NEO="neo4j-community-2.3.2"
+NEO="neo4j-enterprise-3.0.0-RC1"
 
-./${NEO}/bin/neo4j-shell --file cypher/groups.cql
-./${NEO}/bin/neo4j-shell --file cypher/topics.cql
-./${NEO}/bin/neo4j-shell --file cypher/topics_groups.cql
-./${NEO}/bin/neo4j-shell --file cypher/members.cql
-./${NEO}/bin/neo4j-shell --file cypher/members_groups.cql
-./${NEO}/bin/neo4j-shell --file cypher/members_topics.cql
+echo ${PWD}
 
-./${NEO}/bin/neo4j-shell --file cypher/venues.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/groups.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/topics.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/topics_groups.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/members.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/members_groups.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/members_topics.cql
 
-./${NEO}/bin/neo4j-shell --file cypher/events.cql
-./${NEO}/bin/neo4j-shell --file cypher/events_groups.cql
-./${NEO}/bin/neo4j-shell --file cypher/events_venues.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/venues.cql
 
-./${NEO}/bin/neo4j-shell --file cypher/rsvps.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/events.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/events_groups.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/events_venues.cql
 
-./${NEO}/bin/neo4j-shell --file cypher/refactor_rsvp.cql
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/rsvps.cql
+
+./${NEO}/bin/neo4j-shell --file ${PWD}/cypher/refactor_rsvp.cql
 
 # ./${NEO}/bin/neo4j-shell --file cypher/refactor_membership.cql
 # ./${NEO}/bin/neo4j-shell --file cypher/refactor_topic_interest.cql
