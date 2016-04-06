c0628d6ba6074252d068e2e00c747a52fffd24ea
Michael Hunger <github@jexp.de>
2016-04-06 13:22:51 +0200

    Updated guides
    * fixed inconsistencies
    * added missing indices
    * added "index" guide
    * added "Next Steps" at end
    * changed :RSVPD to only include and import "yes"

--- docs/render.sh
+++ docs/render.sh
@@ -1,6 +1,8 @@
 GUIDES=../../neo4j-guides
 # git clone http://github.com/jexp/neo4j-guides $GUIDES
-BASE_URL= http://localhost:8001
+BASE_URL=${1-http://localhost:8001}
+
+$GUIDES/run.sh index.adoc index.html +1 $BASE_URL
 $GUIDES/run.sh 01_similar_groups_by_topic.adoc 01_similar_groups_by_topic.html +1 $BASE_URL
 $GUIDES/run.sh 02_my_similar_groups.adoc 02_my_similar_groups.html +1 $BASE_URL
 $GUIDES/run.sh 03_my_interests.adoc 03_my_interests.html +1 $BASE_URL
