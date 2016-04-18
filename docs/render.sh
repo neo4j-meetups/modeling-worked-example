944e2b4f5cb1c9ec117fe23a806d32864385537d
Mark Needham <mark.needham@neotechnology.com>
2016-04-18 08:47:37 +0100

    adding new adocs into render script

--- docs/render.sh
+++ docs/render.sh
@@ -6,6 +6,7 @@ function render {
 BASE_URL=$1
 $GUIDES/run.sh index.adoc index.html +1 $BASE_URL
 $GUIDES/run.sh 01_similar_groups_by_topic.adoc 01_similar_groups_by_topic.html +1 $BASE_URL
+$GUIDES/run.sh 01_explore_graph_answers.adoc 01_explore_graph_answers.html +1 $BASE_URL
 $GUIDES/run.sh 02_my_similar_groups.adoc 02_my_similar_groups.html +1 $BASE_URL
 $GUIDES/run.sh 02_find_yourself_answers.adoc 02_find_yourself_answers.html +1 $BASE_URL
 $GUIDES/run.sh 03_my_interests.adoc 03_my_interests.html +1 $BASE_URL
@@ -15,7 +16,9 @@ $GUIDES/run.sh 05_venues.adoc 05_venues.html +1 $BASE_URL
 $GUIDES/run.sh 05_venues_import_answers.adoc 05_venues_import_answers.html +1 $BASE_URL
 $GUIDES/run.sh 05_venues_distance_queries_answers.adoc 05_venues_distance_queries_answers.html +1 $BASE_URL
 $GUIDES/run.sh 06_rsvps.adoc 06_rsvps.html +1 $BASE_URL
+$GUIDES/run.sh 06_my_venues_answers.adoc 06_my_venues_answers.html +1 $BASE_URL
 $GUIDES/run.sh 07_procedures.adoc 07_procedures.html +1 $BASE_URL
+$GUIDES/run.sh 07_photos_answers.adoc 07_photos_answers.html +1 $BASE_URL
 $GUIDES/run.sh 08_scoring.adoc 08_scoring.html +1 $BASE_URL
 $GUIDES/run.sh 09_free_for_all.adoc 09_free_for_all.html +1 $BASE_URL
 }
