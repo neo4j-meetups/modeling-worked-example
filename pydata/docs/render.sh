0d68cd46e7497ff76d67bea398c192c0209edec3
Mark Needham <mark.needham@neotechnology.com>
2016-05-05 06:54:31 +0100

    tidying up guides

--- pydata/docs/render.sh
+++ pydata/docs/render.sh
@@ -7,6 +7,7 @@ $GUIDES/run.sh index.adoc index.html +1 "$@"
 $GUIDES/run.sh 01_similar_groups_by_topic.adoc 01_similar_groups_by_topic.html +1 "$@"
 $GUIDES/run.sh 02_clusters.adoc 02_clusters.html +1 "$@"
 $GUIDES/run.sh 03_my_similar_groups.adoc 03_my_similar_groups.html +1 "$@"
+$GUIDES/run.sh 04_events.adoc 04_events.html +1 "$@"
 }
 
 # -a env-training is a flag to enable full content, if you comment it out, the guides are rendered minimally e.g. for a presentation
